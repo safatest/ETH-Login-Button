ad62d6da0286397d9729326180adb8114edf198f
safatest <96929669+safatest@users.noreply.github.com>
2022-01-26 12:45:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// var Web3 = require("web3")
+// import Web3 from web3
+
+const btn = document.getElementById("btn-login");
+async function ethEnabled() {
+    if(btn.innerText == "Web3 Metamask Login"){
+        if (window.ethereum) {
+            // const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+            const accounts = await window.ethereum.send('eth_requestAccounts');
+            window.web3 = new Web3(window.ethereum);
+            // console.log(accounts.result[0])
+            const address = accounts.result[0]
+            // console.log(address)
+            let response = web3.eth.getBalance(address)
+                .then(res => {console.log(res)
+                    // document.getElementById("output").innerHTML = res + " ETH";
+                    btn.innerText = res+"ETH";})
+        return true;
+        }
+        return false;
+    }
+}
+
